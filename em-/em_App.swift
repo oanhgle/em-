c3693521a954a2f8f80bfc9183ba07ca5741842b
oanh <oanhngle@gmail.com>
2023-03-21 01:44:52 -0400

    Initial Commit

---
+++ em-/em_App.swift
@@ -0,0 +1,17 @@
+//
+//  em_App.swift
+//  em-
+//
+//  Created by Oanh Le on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct em_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
